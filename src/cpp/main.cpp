bfc5577ee17ff736dc878cd49f348713852b7cfa
Denis <code4un@yandex.ru>
2018-09-12 18:22:36 +0400

    Added CMake Project
    Added CMake project ;
    Added console hello world ;

---
+++ src/cpp/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2018 Denis Zyamaev. Email: (code4un@yandex.ru)
+ * License: Apache 2.0 (see "LICENSE" file)
+ * Author: Denis Zyamaev (code4un@yandex.ru)
+ * API: C++ 11
+*/
+
+// Include STL
+#include <iostream>
+#include <cstdlib>
+
+/* MAIN */
+int main( int argC, char** argV )
+{
+
+	// Print Hello to Console
+	std::cout << "Octree Example Started" << std::endl;
+
+	// Print Bye to Console
+	std::cout << "Octree Example Finished" << std::endl;
+
+	// Pause Console-Window
+	std::cin.get( );
+
+	// Return OK
+	return( 0 );
+
+}
